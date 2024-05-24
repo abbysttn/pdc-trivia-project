382b1700ea2e2ccae1c727fa12eaded41ed9e6f7
Abby Sutton <qnd0893@autuni.ac.nz>
2024-05-24 20:53:15 +1200

    commit 5/24 fixed startPage and mainPage

--- src/trivia_project/TriviaFrame.java
+++ src/trivia_project/TriviaFrame.java
@@ -5,13 +5,17 @@
 package trivia_project;
 
 import java.awt.CardLayout;
+import java.awt.Dimension;
 
+//commit then push to upstream
 /**
  *
  * @author abbys
  */
 public class TriviaFrame extends javax.swing.JFrame {
 
+    
+    
     /**
      * Creates new form TriviaFrame
      */
@@ -40,8 +44,8 @@ public class TriviaFrame extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         mainPage = new javax.swing.JPanel();
-        jLabel3 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setAlwaysOnTop(true);
@@ -109,44 +113,42 @@ public class TriviaFrame extends javax.swing.JFrame {
         mainPage.setMinimumSize(new java.awt.Dimension(650, 365));
         mainPage.setPreferredSize(new java.awt.Dimension(650, 365));
 
-        jLabel3.setFont(new java.awt.Font("Microsoft YaHei Light", 0, 48)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("Trivia Trekker");
-
-        jButton2.setBackground(new java.awt.Color(200, 213, 185));
-        jButton2.setFont(new java.awt.Font("Microsoft YaHei Light", 0, 24)); // NOI18N
+        jButton2.setBackground(new java.awt.Color(255, 204, 102));
+        jButton2.setFont(new java.awt.Font("Microsoft YaHei Light", 0, 18)); // NOI18N
         jButton2.setForeground(new java.awt.Color(51, 51, 51));
         jButton2.setText("<");
         jButton2.setBorderPainted(false);
+        jButton2.setContentAreaFilled(false);
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
             }
         });
 
+        jLabel3.setFont(new java.awt.Font("Microsoft YaHei Light", 0, 48)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Trivia Trekker");
+
         javax.swing.GroupLayout mainPageLayout = new javax.swing.GroupLayout(mainPage);
         mainPage.setLayout(mainPageLayout);
         mainPageLayout.setHorizontalGroup(
             mainPageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 650, Short.MAX_VALUE)
             .addGroup(mainPageLayout.createSequentialGroup()
                 .addComponent(jButton2)
-                .addGap(144, 144, 144)
-                .addComponent(jLabel3)
-                .addGap(0, 182, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         mainPageLayout.setVerticalGroup(
             mainPageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(mainPageLayout.createSequentialGroup()
-                .addGroup(mainPageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton2)
-                    .addGroup(mainPageLayout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(307, Short.MAX_VALUE))
+                .addComponent(jButton2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(256, Short.MAX_VALUE))
         );
 
-        main.add(mainPage, "card6");
+        main.add(mainPage, "card3");
 
         getContentPane().add(main);
         main.setBounds(0, 0, 0, 0);
@@ -161,6 +163,8 @@ public class TriviaFrame extends javax.swing.JFrame {
         main.add(mainPage);
         main.repaint();
         main.revalidate();
+        Dimension size = getSize();
+        System.out.println("Current size of JFrame: Width = " + size.width + ", Height = " + size.height);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -169,6 +173,8 @@ public class TriviaFrame extends javax.swing.JFrame {
         main.add(startPage);
         main.repaint();
         main.revalidate();
+        Dimension size = getSize();
+        System.out.println("Current size of JFrame: Width = " + size.width + ", Height = " + size.height);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
